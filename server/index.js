698cc19048cce2173ee08da97f9a8d5170e31cf6
Wadie MENDJA <53305297+wadiemendja@users.noreply.github.com>
2020-12-01 16:21:54 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
-const {fetchCovidStatus, filterData} = require('./fetchCovidStatus');
+const { fetchCovidStatus, filterData } = require('./fetchCovidStatus');
 
 const app = express(); // use express
 const dir = path.join(__dirname, '../'); // goes 1 folder back to the given __dirname
 app.use(express.static(dir)); // serving static files
-const port = 3030 || process.env.PORT; // getting the env port
+const port = process.env.PORT || 3030; // getting the env port
 app.listen(port, () => console.log('listening to localhost:' + port)); // listening to the port
 app.get('/api', async function (req, res) {
     const div = await fetchCovidStatus() // fetching data
-                            .then((data) => filterData(data)); // filtering data
-                            console.log(div)
+        .then((data) => filterData(data)); // filtering data
+    console.log(div)
     return res.send(div); // send data
 });
